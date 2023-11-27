5ab62536ef309c94a4a24e0f8b2dbd6e1f857e96
castellano17 <castellano17@gmail.com>
2023-11-26 20:45:22 -0600

    add form

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
-import NewClient from "./pages/NewClient";
+import NewClient, { action as newClientAction } from "./pages/NewClient";
 import Index, { loader as clientesLoader } from "./pages/Index";
 
 const router = createBrowserRouter([
@@ -19,6 +19,7 @@ const router = createBrowserRouter([
       {
         path: "/clientes/nuevo",
         element: <NewClient />,
+        action: newClientAction,
       },
     ],
   },
--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,8 +1,41 @@
+import { useNavigate, Form, useActionData, redirect } from "react-router-dom";
+import Formulario from "../components/Formulario";
+
+export function action() {
+  console.log("enviando datos");
+  return { ok: true };
+}
+
 const NewClient = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h2>nuevo cliente</h2>
-    </div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-3">
+        Llena todos los campos para registrar un nuevo cliente
+      </p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
+      </div>
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <Form method="post" noValidate>
+          <Formulario />
+
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar Cliente"
+          />
+        </Form>
+      </div>
+    </>
   );
 };
 
